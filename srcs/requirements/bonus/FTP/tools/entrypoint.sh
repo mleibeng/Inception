7e2583f5fd8406eadc313d0e2dd4c10ef0178847
Marvin <leibenguth.marvin@web.de>
2024-07-21 00:58:54 +0200

    added FTP server, it works but I need to change the directory it is assigned to in the wordpress volume so i can access themes etc..

--- srcs/requirements/bonus/FTP/tools/entrypoint.sh
+++ srcs/requirements/bonus/FTP/tools/entrypoint.sh
@@ -1,23 +1,45 @@
 #!/bin/sh
 
-. /root/.env
+set -e
 
-if [ -z "$FTPPW" ]; then
-  echo "FTPPW variable is not set in the .env file"
-  exit 1
+echo "Starting entrypoint script..."
+
+if [ ! -f /etc/vsftpd.conf ]; then
+    echo "Error: vsftpd.conf not found!"
+    exit 1
 fi
 
-if [ -z "$USER" ]; then
-  echo "USER variable is not set in the .env file"
-  exit 1
+if [ -z "$FTPPW" ] || [ -z "$USER" ]; then
+    echo "Error: FTPPW or USER variable is not set in the .env file"
+    exit 1
 fi
 
-adduser --disabled-password --gecos "" $USER
+echo "Checking if user $USER exists..."
+if ! id "$USER" &>/dev/null; then
+    echo "Creating user $USER..."
+    adduser --disabled-password --gecos "" $USER
+else
+    echo "User $USER already exists."
+fi
+
+echo "Setting password for $USER..."
 echo "$USER:$FTPPW" | chpasswd
 
-mkdir -p /home/$USER/ftp && \
-chown -R $USER:$USER /home/$USER/ftp && \
-chmod 755 /home/$USER/ftp
+echo "Setting up FTP directories..."
+mkdir -p /home/$USER/ftp
+chown root:root /home/$USER/ftp
+chmod 555 /home/$USER/ftp
+
+mkdir -p /home/$USER/ftp/files
+chown $USER:$USER /home/$USER/ftp/files
+chmod 775 /home/$USER/ftp/files
+
+echo "Ensuring secure chroot directory exists..."
+mkdir -p /var/run/vsftpd/empty
+
+echo "Creating userlist..."
+echo "$USER" > /etc/vsftpd.userlist
+chmod 644 /etc/vsftpd.userlist
 
 echo "Starting vsftpd..."
-/usr/sbin/vsftpd /etc/vsftpd.conf
+exec /usr/sbin/vsftpd /etc/vsftpd.conf
