48df030ade3e29f7833456396ee2d6f37550eec0
mleibeng <mleibeng@student.42.fr>
2024-07-21 21:22:11 +0200

    Added the hopefully correct directory to the ftp service to add files directly into wordpress themes or configs etc...

--- srcs/requirements/bonus/FTP/tools/entrypoint.sh
+++ srcs/requirements/bonus/FTP/tools/entrypoint.sh
@@ -5,34 +5,25 @@ set -e
 echo "Starting entrypoint script..."
 
 if [ ! -f /etc/vsftpd.conf ]; then
-    echo "Error: vsftpd.conf not found!"
-    exit 1
+	echo "Error: vsftpd.conf not found!"
+	exit 1
 fi
 
 if [ -z "$FTPPW" ] || [ -z "$USER" ]; then
-    echo "Error: FTPPW or USER variable is not set in the .env file"
-    exit 1
+	echo "Error: FTPPW or USER variable is not set in the .env file"
+	exit 1
 fi
 
 echo "Checking if user $USER exists..."
 if ! id "$USER" &>/dev/null; then
-    echo "Creating user $USER..."
-    adduser --disabled-password --gecos "" $USER
+	echo "Creating user $USER..."
+	adduser --disabled-password --gecos "" $USER
 else
-    echo "User $USER already exists."
+	echo "User $USER already exists."
 fi
 
 echo "Setting password for $USER..."
 echo "$USER:$FTPPW" | chpasswd
-
-echo "Setting up FTP directories..."
-mkdir -p /home/$USER/ftp
-chown root:root /home/$USER/ftp
-chmod 555 /home/$USER/ftp
-
-mkdir -p /home/$USER/ftp/files
-chown $USER:$USER /home/$USER/ftp/files
-chmod 775 /home/$USER/ftp/files
 
 echo "Ensuring secure chroot directory exists..."
 mkdir -p /var/run/vsftpd/empty
